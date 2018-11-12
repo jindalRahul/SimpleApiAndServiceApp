93c2738d17f53dc2dbcbd3627c21b0a2c1f1e620
Rahul Jindal <rahuljindal121@gmail.com>
2018-11-12 22:43:15 +0530

    initial commit

--- app/src/main/java/com/rahul/simpleapiapp/MyAppGlideModule.java
+++
@@ -1,9 +0,0 @@
-package com.rahul.simpleapiapp;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-
-@GlideModule
-public final class MyAppGlideModule extends AppGlideModule {
-}
